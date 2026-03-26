54236108413409b65152eb2968ca54e1d6e46c70
Aissa Boukraa "Bejaia <Aissa.Bo@Techno-dz.com>
2026-03-26 13:17:15 +0100

    Mise à jour du projet

--- lib/features/import_export/data/services/google_sheets_service.dart
+++ lib/features/import_export/data/services/google_sheets_service.dart
@@ -1,5 +1,4 @@
 import 'package:drift/drift.dart';
-import 'package:inventory_manager/database/app_database.dart'; // adapte le chemin
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
